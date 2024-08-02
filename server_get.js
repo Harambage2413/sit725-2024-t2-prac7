463c7c2cec7818770e9424a59325b2872fb18d17
Your Name <s223882551@deakin.edu.au>
2024-08-02 15:28:26 +1000

    first commit

--- server_get.js
+++ server_get.js
@@ -1,5 +1,8 @@
+const path = require('path');
 const express= require("express");
 const app= express();
+app.use(express.static('public'))
+// app.use(express.static(path.join(__dirname, 'public')))
 const addTwoNumber= (n1,n2)=>{
     return n1+n2;
 }
