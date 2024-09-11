b1a15cf274d317b0a656b853708e829831a8b719
Your Name <s223882551@deakin.edu.au>
2024-09-11 15:01:28 +1000

    Task 7

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ let express = require('express');
 let app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://senehebalasuriya123:YbHg1WSR30KZKi6w@cluster0.dhrdt.mongodb.net/testApp?retryWrites=true&w=majority"
-let port = process.env.port || 3040;
+let port = process.env.port || 3000;
 let collection;
+const { Socket } = require('socket.io');
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+
+
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -22,40 +27,53 @@ async function runDBConnection() {
     try {
         await client.connect();
         collection = client.db().collection('Pictures');
-        // console.log(collection);
-    } catch(ex) {
+        console.log("collection");
+    } catch (ex) {
         console.error(ex);
     }
 }
 
-app.get('/api/pictures', async(req,res) => {
-    try{
-    const response= await collection.find().sort({"_id":-1}).limit(1).toArray()
-    console.log(response);
-    res.json({statusCode:200, data:response[0], message:'success'});
-    }catch(error){
+app.get('/api/pictures', async (req, res) => {
+    try {
+        const response = await collection.find().sort({ "_id": -1 }).limit(1).toArray()
+        res.json({ statusCode: 200, data: response[0], message: 'success' });
+    } catch (error) {
         console.log(error);
-        res.json({statusCode:500, data:null, message:'fail'});
+        res.json({ statusCode: 500, data: null, message: 'fail' });
     }
 });
 
-app.post('/api/pictures', async (req,res)=>{
+app.post('/api/pictures', async (req, res) => {
     let pictureData = req.body;
-    try{
+    try {
         const response = await collection.insertOne(pictureData);
-        console.log(response);
-        res.json({statusCode:201, data:response, message:'success'});
-    }catch(error){
+        // console.log(response);
+        res.json({ statusCode: 201, data: response, message: 'success' });
+    } catch (error) {
         console.log(error);
-        res.json({statusCode:422, data:null, message:'fail'});
-        
+        res.json({ statusCode: 422, data: null, message: 'fail' });
+
     }
 });
 
 
-    runDBConnection();
+runDBConnection();
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    setInterval(() => {
+        x = parseInt(Math.random() * 10);
+        socket.emit('number', x);
+        console.log('Emmiting Number ' + x);
+    }, 1000)
+});
 
 
-app.listen(port, ()=>{
+
+http.listen(port, () => {
     console.log(`express server started ${port}`);
 });
